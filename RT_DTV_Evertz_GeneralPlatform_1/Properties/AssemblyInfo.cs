3a73fb0cbed9de949cc1583d2ebb88408ae75782
Alex Johnson <llalexjohnsonll@gmail.com>
2025-06-18 11:34:23 -0500

    test complete

---
+++ RT_DTV_Evertz_GeneralPlatform_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RT_DTV_Evertz_GeneralPlatform_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("RT_DTV_Evertz_GeneralPlatform_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("977A06BD-5126-43D9-B334-7A6068CC862C")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ RT_DTV_Evertz_GeneralPlatform_1/TestCases/BPXRFSatelliteSwitching.cs
@@ -0,0 +1,104 @@
+﻿namespace RT_DTV_Evertz_GeneralPlatform_1.TestCases
+{
+	using System;
+	using System.Collections.Generic;
+	using Library.Tests.TestCases;
+	using QAPortalAPI.Models.ReportingModels;
+	using Skyline.DataMiner.Automation;
+	using Skyline.DataMiner.Core.DataMinerSystem.Common;
+
+	public class BPXRFSatelliteSwitching : ITestCase
+	{
+		private readonly int modeWritePid = 7353;
+		private readonly int activeChannelPid = 7312;
+
+		public BPXRFSatelliteSwitching(string name, string elementName)
+		{
+			Name = name;
+			ElementName = elementName;
+		}
+
+		public enum BpxrfModes
+		{
+			External = 1,
+			AutoSwitchBack = 2,
+			Auto = 3,
+			ChannelA = 4,
+			ChannelB = 5,
+		}
+
+		public enum BpxrfActiveChannels
+		{
+			ChannelA = 1,
+			ChannelB = 2,
+		}
+
+		public string Name { get; set; }
+
+		public string Index { get; set; }
+
+		public string ElementName { get; set; }
+
+		public PerformanceTestCaseReport PerformanceTestCaseReport { get; }
+
+		public TestCaseReport TestCaseReport { get; private set; }
+
+		private List<ColumnFilter> UpdatedStreamFilter { get; set; }
+
+		public void Execute(IEngine engine)
+		{
+			try
+			{
+				if (RunTestCaseSatelliteSwitching(engine))
+				{
+					TestCaseReport.GetSuccessTestCase(Name);
+				}
+				else
+				{
+					var failDescription = $"Could not validate new crosspoint data with outputs table.";
+					TestCaseReport.GetFailTestCase(Name, failDescription);
+				}
+
+			}
+			catch (Exception e)
+			{
+				TestCaseReport = TestCaseReport.GetFailTestCase($"Foreign Keys validation for {Name}", $"Exception: {e}");
+			}
+		}
+
+		private bool RunTestCaseSatelliteSwitching(IEngine engine, int maxAttempts = 3)
+		{
+			var valueToSet = 0;
+			var element = engine.FindElement(ElementName);
+
+			if (element != null)
+			{
+				var activeChannel = Convert.ToInt32(element.GetParameter(activeChannelPid));
+				if (activeChannel == (int)BpxrfActiveChannels.ChannelA)
+				{
+					valueToSet = (int)BpxrfModes.ChannelB;
+				}
+				else
+				{
+					valueToSet = (int)BpxrfModes.ChannelA;
+				}
+
+				if (valueToSet != 0)
+				{
+					element.SetParameter(modeWritePid, valueToSet);
+					System.Threading.Thread.Sleep(3000);
+
+					var expectedValue = valueToSet - 3;
+					activeChannel = Convert.ToInt32(element.GetParameter(activeChannelPid));
+
+					if (activeChannel == expectedValue)
+					{
+						return true;
+					}
+				}
+			}
+
+			return false;
+		}
+	}
+}
